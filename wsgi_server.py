972bcee2f9b2217e7ca1596d6ad5aaf6efafa85d
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-14 20:31:04 -0300

    Create first middleware

--- wsgi_server.py
+++ wsgi_server.py
@@ -1,8 +1,9 @@
 from wsgiref.simple_server import make_server
 
+from middleware import Reverseware
 from webapp import application
 
 
 if __name__ == "__main__":
-    server = make_server('localhost', 8000, app=application)
+    server = make_server('localhost', 8000, app=Reverseware(application))
     server.serve_forever()
