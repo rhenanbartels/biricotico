b49b6f89f9efa75f812f8f92d803025bcdc2f218
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-14 20:54:55 -0300

    Start biricotico webframework

---
+++ biricotico/app.py
@@ -0,0 +1,9 @@
+"""
+WSGI-compatible interface
+"""
+
+def app(environ, start_response):
+    response_body = b"Hello, World!"
+    status = "200 OK"
+    start_response(status, headers=[])
+    return iter([response_body])
