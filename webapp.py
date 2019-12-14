0765613594a88bcf7a3829bf38db3c06e03dcd94
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-14 20:20:52 -0300

    Implement wsgi callable

--- webapp.py
+++ webapp.py
@@ -1,2 +1,14 @@
 def application(environ, start_response):
-    pass
+    response_body = [
+        f'{key}: {value}' for key, value in sorted(environ.items())
+    ]
+    response_body = '\n'.join(response_body)
+    status = '200 OK'
+
+    response_headers = [
+        ('Content-type', 'text/plain')
+    ]
+
+    start_response(status, response_headers)
+
+    return [response_body.encode('utf-8')]
