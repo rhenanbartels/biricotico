7ef6889c544ac86096695eed5163f96e482afe29
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-14 21:13:01 -0300

    Use WebOb Request and Response objects

--- biricotico/api.py
+++ biricotico/api.py
@@ -1,6 +1,9 @@
+from webob import Request, Response
+
+
 class API:
     def __call__(self, environ, start_response):
-        response_body = b"Hello, World"
-        status = "200 OK"
-        start_response(status, headers=[])
-        return iter([response_body])
+        request = Request(environ)
+        response = Response()
+        response.body = b"Hello, World"
+        return response(environ, start_response)
