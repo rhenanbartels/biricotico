dc1e3e1197db8bb1237a1071ebabd053a3cb9747
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-16 19:30:38 -0300

    Add routing

--- biricotico/api.py
+++ biricotico/api.py
@@ -2,13 +2,25 @@ from webob import Request, Response
 
 
 class API:
+    def __init__(self):
+        self.taps = {}
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
     def handle_request(self, request):
-        user_agent = request.environ.get('HTTP_USER_AGENT', 'No user agent')
         response = Response()
-        response.text = f'Hello, my friend using {user_agent}'
-        return response
+
+        for path, handler in self.taps.items():
+            if path == request.path:
+                handler(request, response)
+                return response
+
+    def tap(self, path):
+        def wrapper(handler):
+            self.taps[path] = handler
+            return handler
+
+        return wrapper
--- biricotico/app.py
+++ biricotico/app.py
@@ -5,3 +5,18 @@ from biricotico.api import API
 
 
 app = API()
+
+
+@app.tap('/')
+def landing_page(request, response):
+    response.text = 'Welcome!'
+
+
+@app.tap('/home')
+def home(request, response):
+    response.text = 'Hello from the HOME page'
+
+
+@app.tap('/about')
+def about(request, response):
+    response.text = 'Hello from the ABOUT page'
