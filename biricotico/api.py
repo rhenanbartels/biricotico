d610761511fe2aff496508711d1f774fe1ab69af
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-16 19:20:39 -0300

    Create handle_request method

--- biricotico/api.py
+++ biricotico/api.py
@@ -4,6 +4,11 @@ from webob import Request, Response
 class API:
     def __call__(self, environ, start_response):
         request = Request(environ)
-        response = Response()
-        response.body = b"Hello, World"
+        response = self.handle_request(request)
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        user_agent = request.environ.get('HTTP_USER_AGENT', 'No user agent')
+        response = Response()
+        response.text = f'Hello, my friend using {user_agent}'
+        return response
