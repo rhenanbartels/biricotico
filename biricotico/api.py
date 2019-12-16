04087506853e6715e85bd479b55adac21b91f313
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-16 19:36:34 -0300

    Refactor code

--- biricotico/api.py
+++ biricotico/api.py
@@ -10,16 +10,21 @@ class API:
         response = self.handle_request(request)
         return response(environ, start_response)
 
+    def find_handler(self, request):
+        for path, handler in self.taps.items():
+            if path == request.path:
+                return handler
+
     def handle_request(self, request):
         response = Response()
 
-        for path, handler in self.taps.items():
-            if path == request.path:
-                handler(request, response)
-                return response
-
+        handler = self.find_handler(request)
+        if handler is not None:
+            handler(request, response)
+        else:
             self.default_response(response)
-            return response
+
+        return response
 
     def default_response(self, response):
         response.status = 404
