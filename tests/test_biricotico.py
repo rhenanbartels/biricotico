5a4e7576d13ba0297baf89aa0c742d62eaaefdcb
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-24 13:41:22 -0300

    Add test for repeating routes

--- tests/test_biricotico.py
+++ tests/test_biricotico.py
@@ -12,3 +12,8 @@ def test_basic_route_adding(api):
     @api.tap("/home")
     def home(req, resp):
         resp.text = "TEST"
+
+        with pytest.raises(AssertionError):
+            @api.tap("/home")
+            def home2(req, resp):
+                resp.text = "TEST"
